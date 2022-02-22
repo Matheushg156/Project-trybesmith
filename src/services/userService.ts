e205774d3b7b4632d8657b2c56f875157c032ecc
Matheus Henrique Gonzaga <matheushg@pop-os.localdomain>
2022-02-22 16:16:06 -0300

    Cria a função de validar classe e level no service

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -13,6 +13,34 @@ const validateUserName = (userName: string): Error | false => {
   return false;
 };
 
+const validateClasse = (classe: string): Error | false => {
+  if (!classe) {
+    return { code: 400, error: 'Classe is required' };
+  }
+  if (typeof classe !== 'string') {
+    return { code: 422, error: 'Classe must be a string' };
+  }
+  if (classe.length < 3) {
+    return { code: 422, error: 'Classe must be longer than 2 characters' };
+  }
+  return false;
+};
+
+const validateLevel = (level: number): Error | false => {
+  if (!level) {
+    return { code: 400, error: 'Level is required' };
+  }
+  if (typeof level !== 'number') {
+    return { code: 422, error: 'Level must be a number' };
+  }
+  if (level < 1) {
+    return { code: 422, error: 'Level must be greater than 0' };
+  }
+  return false;
+};
+
 export default {
   validateUserName,
+  validateClasse,
+  validateLevel,
 };
