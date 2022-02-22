347219e321225f6f5e1b110022763a281dc54da1
Matheus Henrique Gonzaga <matheushg@pop-os.localdomain>
2022-02-22 16:19:08 -0300

    Cria a função de validar password no service

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -39,8 +39,22 @@ const validateLevel = (level: number): Error | false => {
   return false;
 };
 
+const validatePassword = (password: string): Error | false => {
+  if (!password) {
+    return { code: 400, error: 'Password is required' };
+  }
+  if (typeof password !== 'string') {
+    return { code: 422, error: 'Password must be a string' };
+  }
+  if (password.length < 8) {
+    return { code: 422, error: 'Password must be longer than 7 characters' };
+  }
+  return false;
+};
+
 export default {
   validateUserName,
   validateClasse,
   validateLevel,
+  validatePassword,
 };
