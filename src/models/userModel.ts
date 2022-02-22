8b3c65982a2ab48d0159cc727bd0162b6fe9b01c
Matheus Henrique Gonzaga <matheushg@pop-os.localdomain>
2022-02-22 15:36:03 -0300

    Cria a função create no userModel

---
+++ src/models/userModel.ts
@@ -0,0 +1,24 @@
+import { ResultSetHeader } from 'mysql2';
+import connection from './connection';
+import { User, UserWithId } from './UsersInterface';
+
+const create = async (user: User): Promise<UserWithId> => {
+  const { username, classe, level, password } = user;
+  const query = `
+    INSERT INTO Trybesmith.Users (username, classe, level, password)
+    VALUES (?, ?, ?, ?)
+  `;
+  const [result] = await connection.execute<ResultSetHeader>(
+    query, 
+    [username, classe, level, password],
+  );
+
+  return {
+    id: result.insertId,
+    ...user,
+  };
+};
+
+export default {
+  create,
+};
