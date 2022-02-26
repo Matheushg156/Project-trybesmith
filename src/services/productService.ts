719fdb95508874eeca3052b16647fba3d5215b5f
Matheus Henrique Gonzaga <matheushg@pop-os.localdomain>
2022-02-26 15:16:41 -0300

    Cria a função validateAmount no service product

--- src/services/productService.ts
+++ src/services/productService.ts
@@ -14,6 +14,20 @@ const validateName = (name: string): Error | false => {
   return false;
 };
 
+const validateAmount = (amount: string): Error | false => {
+  if (!amount) {
+    return { code: 400, error: 'Amount is required' };
+  }
+  if (typeof amount !== 'string') {
+    return { code: 422, error: 'Amount must be a string' };
+  }
+  if (amount.length < 3) {
+    return { code: 422, error: 'Amount must be longer than 2 characters' };
+  }
+  return false;
+};
+
 export default {
   validateName,
+  validateAmount,
 };
