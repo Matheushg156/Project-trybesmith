218acfb26c18c6dd7c877c92c9dc927fe1f85277
Matheus Henrique Gonzaga <matheushg@pop-os.localdomain>
2022-03-01 12:20:45 -0300

    Cria a função create no controller order do req 05

--- src/controllers/orderController.ts
+++ src/controllers/orderController.ts
@@ -1,5 +1,6 @@
 import { Request, Response, NextFunction } from 'express';
 import OrderService from '../services/orderService';
+import OrderModel from '../models/orderModel';
 
 const validateOrder = (req: Request, res: Response, next: NextFunction) => {
   const { order } = req.body;
@@ -10,6 +11,16 @@ const validateOrder = (req: Request, res: Response, next: NextFunction) => {
   next();
 };
 
+const create = async (req: Request, res: Response) => {
+  const { dataValues: { id: userId } } = req.user;
+  const order = await OrderModel.create(req.body, userId);
+  if (order) {
+    return res.status(201).json({ order });
+  }
+  return res.status(500).json({ error: 'Internal server error' });
+};
+
 export default {
   validateOrder,
+  create,
 };
