7abc6b7646c7c75cf7b2e31176d3bad6b1fc7ec3
Matheus Henrique Gonzaga <matheushg@pop-os.localdomain>
2022-03-01 12:27:48 -0300

    Cria a rota post order do req 05 no app

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import express from 'express';
 import userRouter from './routes/userRoutes';
 import loginRouter from './routes/loginRoutes';
 import productRouter from './routes/productRoutes';
+import orderRouter from './routes/orderRoutes';
 
 const app = express();
 
@@ -11,5 +12,6 @@ app.use(express.json());
 app.use('/users', userRouter);
 app.use('/login', loginRouter);
 app.use('/products', productRouter);
+app.use('/orders', orderRouter);
 
 export default app;
